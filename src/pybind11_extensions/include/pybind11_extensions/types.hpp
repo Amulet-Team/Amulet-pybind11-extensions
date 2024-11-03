c2be08fdc8c99c0ddbca05d6fe91294c99cbd0b9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-03 13:28:57 +0000

    Simplified NotImplementedType

--- src/pybind11_extensions/include/pybind11_extensions/types.hpp
+++ src/pybind11_extensions/include/pybind11_extensions/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <pybind11/pybind11.h>
+#include "builtins.hpp"
 
 // This extension adds a py::object subclass for types.NotImplementedType.
 // This is used as a return in comparison operators.
@@ -7,19 +8,6 @@
 
 namespace pybind11_extensions {
     namespace types {
-        class NotImplementedType : public pybind11::object {
-            PYBIND11_OBJECT_DEFAULT(NotImplementedType, object, PyObject_Type)
-            using object::object;
-        };
+        using NotImplementedType = pybind11_extensions::builtins::PyObjectStr<"types.NotImplementedType">;
     }
-}
-
-
-namespace pybind11 {
-	namespace detail {
-		template <>
-		struct handle_type_name<pybind11_extensions::types::NotImplementedType> {
-			static constexpr auto name = const_name("types.NotImplementedType");
-		};
-	}
 }
