822d6a3191fa285fd1e9a082a6f99995a48bbf3c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-01 12:03:57 +0000

    Refactored iterable and iterator

--- src/pybind11_extensions/include/pybind11_extensions/collections.hpp
+++ src/pybind11_extensions/include/pybind11_extensions/collections.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <iterator>
+#include <ranges>
+
 #include <pybind11/pybind11.h>
 
 // I have found cases where I want to accept or return a python object matching a collections.abc class.
@@ -7,41 +10,91 @@
 // Note that these are handled in the same way as py::object thus there is no type validation.
 
 namespace pybind11_extensions {
+
 template <typename T>
-class Iterator : public pybind11::iterator {
+class Iterator : public pybind11::object {
 public:
-    using pybind11::iterator::iterator;
+    using iterator_category = std::input_iterator_tag;
+    using difference_type = pybind11::ssize_t;
+    using value_type = T;
+
+    PYBIND11_OBJECT_DEFAULT(Iterator, object, PyIter_Check)
+
+    Iterator& operator++()
+    {
+        advance();
+        return *this;
+    }
+
+    Iterator operator++(int)
+    {
+        auto rv = *this;
+        advance();
+        return rv;
+    }
 
     T operator*() const
     {
-        return pybind11::iterator::operator*().template cast<T>();
+        return obj().cast<T>();
     }
 
-    std::unique_ptr<T> operator->() const
+    static Iterator sentinel() { return {}; }
+
+    friend bool operator==(const Iterator& a, const Iterator& b) { return a.obj().ptr() == b.obj().ptr(); }
+    friend bool operator!=(const Iterator& a, const Iterator& b) { return a.obj().ptr() != b.obj().ptr(); }
+
+private:
+    void advance()
     {
-        return std::make_unique<T>(operator*());
+        value = pybind11::reinterpret_steal<pybind11::object>(PyIter_Next(m_ptr));
+        if (value.ptr() == nullptr && PyErr_Occurred()) {
+            throw pybind11::error_already_set();
+        }
     }
+
+    const pybind11::object& obj() const
+    {
+        if (m_ptr && !value.ptr()) {
+            auto& self = const_cast<Iterator&>(*this);
+            self.advance();
+        }
+        return value;
+    }
+
+private:
+    pybind11::object value = {};
 };
+static_assert(std::input_iterator<Iterator<int>>);
+
 
 template <typename T>
-class Iterable : public pybind11::iterable {
-public:
-    using pybind11::iterable::iterable;
+class Iterable : public pybind11::object {
+    PYBIND11_OBJECT_DEFAULT(Iterable, object, pybind11::detail::PyIterable_Check)
+    
     Iterator<T> begin() const
     {
-        return Iterator<T>(pybind11::iterable::begin());
+        return Iterator<T>(pybind11::object::begin());
     }
     Iterator<T> end() const
     {
-        return Iterator<T>(pybind11::iterable::end());
+        return Iterator<T>(pybind11::object::end());
     }
+
 };
+static_assert(std::ranges::input_range<Iterable<int>>);
+static_assert(std::convertible_to<std::ranges::range_value_t<Iterable<int>>, const int&>);
 
 namespace collections {
     namespace abc {
         template <typename T>
         class Iterator : public pybind11::object {
             PYBIND11_OBJECT_DEFAULT(Iterator, object, PyObject_Type)
+            using object::object;
+        };
+
+        template <typename T>
+        class Iterable : public pybind11::object {
+            PYBIND11_OBJECT_DEFAULT(Iterable, object, PyObject_Type)
             using object::object;
         };
 
@@ -99,6 +152,11 @@ namespace detail {
     template <typename T>
     struct handle_type_name<pybind11_extensions::Iterator<T>> {
         static constexpr auto name = const_name("collections.abc.Iterator[") + make_caster<T>::name + const_name("]");
+    };
+
+    template <typename T>
+    struct handle_type_name<pybind11_extensions::collections::abc::Iterable<T>> {
+        static constexpr auto name = const_name("collections.abc.Iterable[") + make_caster<T>::name + const_name("]");
     };
 
     template <>
