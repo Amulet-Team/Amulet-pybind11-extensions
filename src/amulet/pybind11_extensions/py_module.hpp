d60aeca118c4bd0fdb1c9488a99be3123d47167f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-23 16:26:04 +0100

    Remove set_package_path

--- src/amulet/pybind11_extensions/py_module.hpp
+++ src/amulet/pybind11_extensions/py_module.hpp
@@ -32,11 +32,5 @@ namespace pybind11_extensions {
         detail::def_package_path(m_parent, m, name);
         return m;
     }
-
-    // __path__ is not defined on the root module until after the pybind11 module constructor finishes.
-    // This function patches in the root __path__ to enable def_subpackage to access it.
-    inline void set_package_path(pybind11::module m, std::string module_name) {
-        m.attr("__path__") = pybind11::module::import("importlib.util").attr("find_spec")(module_name).attr("submodule_search_locations");
-    }
 } // namespace pybind11_extensions
 } // namespace Amulet
