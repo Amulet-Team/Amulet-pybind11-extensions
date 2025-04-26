b4466f8e5c76098dca59e42b5208bf3ec06ce62f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-26 15:54:50 +0100

    Add mutable mapping tests

---
+++ src/amulet/pybind11_extensions/mutable_mapping.hpp
@@ -0,0 +1,152 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+#include "mapping.hpp"
+
+namespace py = pybind11;
+
+namespace Amulet {
+namespace pybind11_extensions {
+    namespace collections {
+        template <typename KT = pybind11::object, typename VT = pybind11::object, typename clsT>
+        void def_MutableMapping_pop(clsT cls)
+        {
+            pybind11::object marker = pybind11::module::import("builtins").attr("Ellipsis");
+            cls.def(
+                "pop",
+                [marker](
+                    pybind11::object self,
+                    pybind11_extensions::PyObjectCpp<KT> key,
+                    pybind11_extensions::PyObjectCpp<VT> default_) -> pybind11_extensions::PyObjectCpp<VT> {
+                    pybind11::object value;
+                    try {
+                        value = self.attr("__getitem__")(key);
+                    } catch (const pybind11::error_already_set& e) {
+                        if (e.matches(PyExc_KeyError)) {
+                            if (default_.is(marker)) {
+                                throw;
+                            }
+                            return default_;
+                        } else {
+                            throw;
+                        }
+                    }
+                    self.attr("__delitem__")(key);
+                    return value;
+                },
+                pybind11::arg("key"),
+                pybind11::arg("default") = marker);
+        }
+
+        template <typename KT = pybind11::object, typename VT = pybind11::object, typename clsT>
+        void def_MutableMapping_popitem(clsT cls)
+        {
+            pybind11::object iter = pybind11::module::import("builtins").attr("iter");
+            pybind11::object next = pybind11::module::import("builtins").attr("next");
+            cls.def(
+                "popitem",
+                [iter, next](pybind11::object self) -> std::pair<
+                                                        pybind11_extensions::PyObjectCpp<KT>,
+                                                        pybind11_extensions::PyObjectCpp<VT>> {
+                    pybind11::object key;
+                    try {
+                        key = next(iter(self));
+                    } catch (const pybind11::error_already_set& e) {
+                        if (e.matches(PyExc_StopIteration)) {
+                            throw pybind11::key_error();
+                        } else {
+                            throw;
+                        }
+                    }
+                    pybind11::object value = self.attr("__getitem__")(key);
+                    self.attr("__delitem__")(key);
+                    return std::make_pair(key, value);
+                });
+        }
+
+        template <typename clsT>
+        void def_MutableMapping_clear(clsT cls)
+        {
+            cls.def(
+                "clear",
+                [](pybind11::object self) {
+                    try {
+                        while (true) {
+                            self.attr("popitem")();
+                        }
+                    } catch (const pybind11::error_already_set& e) {
+                        if (!e.matches(PyExc_KeyError)) {
+                            throw;
+                        }
+                    }
+                });
+        }
+
+        template <typename clsT>
+        void def_MutableMapping_update(clsT cls)
+        {
+            pybind11::object isinstance = pybind11::module::import("builtins").attr("isinstance");
+            pybind11::object hasattr = pybind11::module::import("builtins").attr("hasattr");
+            pybind11::object PyMapping = pybind11::module::import("collections.abc").attr("Mapping");
+            cls.def(
+                "update",
+                [isinstance,
+                    hasattr,
+                    PyMapping](
+                    pybind11::object self,
+                    pybind11::object other,
+                    pybind11::kwargs kwargs) {
+                    if (py::hasattr(other, "keys")) {
+                        pybind11::object keys = other.attr("keys")();
+                        for (auto it = keys.begin(); it != keys.end(); it++) {
+                            self.attr("__setitem__")(*it, other.attr("__getitem__")(*it));
+                        }
+                    } else {
+                        for (auto it = other.begin(); it != other.end(); it++) {
+                            self.attr("__setitem__")(
+                                it->attr("__getitem__")(0),
+                                it->attr("__getitem__")(1));
+                        }
+                    }
+                    pybind11::object items = kwargs.attr("items")();
+                    for (auto it = items.begin(); it != items.end(); it++) {
+                        self.attr("__setitem__")(
+                            it->attr("__getitem__")(0),
+                            it->attr("__getitem__")(1));
+                    }
+                },
+                pybind11::arg("other") = pybind11::tuple());
+        }
+
+        template <typename KT = pybind11::object, typename VT = pybind11::object, typename clsT>
+        void def_MutableMapping_setdefault(clsT cls)
+        {
+            cls.def(
+                "setdefault",
+                [](
+                    pybind11::object self,
+                    pybind11_extensions::PyObjectCpp<KT> key,
+                    pybind11::typing::Optional<VT> default_ = pybind11::none()) -> pybind11::typing::Optional<VT> {
+                    try {
+                        return self.attr("__getitem__")(key);
+                    } catch (const pybind11::error_already_set& e) {
+                        if (e.matches(PyExc_KeyError)) {
+                            self.attr("__setitem__")(key, default_);
+                        } else {
+                            throw;
+                        }
+                    }
+                    return default_;
+                });
+        }
+
+        template <typename clsT>
+        void register_MutableMapping(clsT cls)
+        {
+            pybind11::module::import("collections.abc").attr("MutableMapping").attr("register")(cls);
+        }
+
+    } // namespace collections
+} // namespace pybind11_extensions
+} // namespace Amulet
---
+++ tests/test_mutable_mapping.py
@@ -0,0 +1,111 @@
+import unittest
+from collections.abc import Mapping, MutableMapping
+
+
+class MutableMappingTestCase(unittest.TestCase):
+    def test_mutable_mapping(self) -> None:
+        from _test_mutable_mapping import TestMutableMapping
+
+        mapping = TestMutableMapping({"1": 2, "3": 4, "5": 6})
+
+        self.assertEqual(2, mapping["1"])
+        self.assertEqual(4, mapping["3"])
+        self.assertEqual(6, mapping["5"])
+        with self.assertRaises(KeyError):
+            _ = mapping["0"]
+
+        self.assertEqual(3, len(mapping))
+
+        it = iter(mapping)
+        self.assertEqual("1", next(it))
+        self.assertEqual("3", next(it))
+        self.assertEqual("5", next(it))
+        with self.assertRaises(StopIteration):
+            next(it)
+
+        self.assertEqual("{'1': 2, '3': 4, '5': 6}", repr(mapping))
+
+        self.assertNotIn("0", mapping)
+        self.assertIn("1", mapping)
+        self.assertNotIn("2", mapping)
+        self.assertIn("3", mapping)
+        self.assertNotIn("4", mapping)
+        self.assertIn("5", mapping)
+        self.assertNotIn("6", mapping)
+
+        self.assertEqual({"1", "3", "5"}, mapping.keys())
+        self.assertEqual({2, 4, 6}, set(mapping.values()))
+        self.assertEqual({("1", 2), ("3", 4), ("5", 6)}, set(mapping.items()))
+
+        self.assertIsNone(mapping.get("0"))
+        self.assertEqual(2, mapping.get("1"))
+        self.assertIsNone(mapping.get("2"))
+        self.assertEqual(4, mapping.get("3"))
+        self.assertIsNone(mapping.get("4"))
+        self.assertEqual(6, mapping.get("5"))
+        self.assertIsNone(mapping.get("6"))
+        self.assertEqual("hello world", mapping.get("6", "hello world"))
+
+        self.assertEqual(
+            TestMutableMapping({"1": 2, "3": 4, "5": 6}),
+            TestMutableMapping({"1": 2, "3": 4, "5": 6}),
+        )
+        self.assertNotEqual(
+            TestMutableMapping({"1": 2, "3": 4, "5": 6}),
+            TestMutableMapping({"1": 2, "3": 4, "5": 7}),
+        )
+        self.assertNotEqual(
+            TestMutableMapping({"1": 2, "3": 4, "5": 6}), TestMutableMapping({})
+        )
+        self.assertNotEqual(
+            TestMutableMapping({}), TestMutableMapping({"1": 2, "3": 4, "5": 7})
+        )
+
+        with self.assertRaises(TypeError):
+            hash(mapping)
+
+        self.assertIsInstance(mapping, Mapping)
+        self.assertIsInstance(mapping, MutableMapping)
+
+        mapping["7"] = 8
+        self.assertEqual(TestMutableMapping({"1": 2, "3": 4, "5": 6, "7": 8}), mapping)
+        self.assertEqual(8, mapping["7"])
+        del mapping["7"]
+        self.assertEqual(TestMutableMapping({"1": 2, "3": 4, "5": 6}), mapping)
+
+        with self.assertRaises(KeyError):
+            mapping.pop("0")
+        self.assertEqual("hello world", mapping.pop("0", "hello world"))
+        self.assertEqual(2, mapping.pop("1"))
+        self.assertEqual(TestMutableMapping({"3": 4, "5": 6}), mapping)
+        self.assertEqual(6, mapping.pop("5"))
+        self.assertEqual(TestMutableMapping({"3": 4}), mapping)
+        self.assertEqual(("3", 4), mapping.popitem())
+        with self.assertRaises(KeyError):
+            mapping.popitem()
+
+        mapping = TestMutableMapping({"1": 2, "3": 4, "5": 6})
+        mapping.clear()
+        self.assertEqual(TestMutableMapping({}), mapping)
+
+        mapping.update({"10": 20}, a=15)
+        mapping.update([("30", 40)], b=35)
+        self.assertEqual(
+            TestMutableMapping({"10": 20, "30": 40, "a": 15, "b": 35}), mapping
+        )
+        mapping.update({"10": 21}, a=16)
+        mapping.update([("30", 41)], b=36)
+        self.assertEqual(
+            TestMutableMapping({"10": 21, "30": 41, "a": 16, "b": 36}), mapping
+        )
+
+        mapping.setdefault("10", 22)
+        mapping.setdefault("11", 11)
+        self.assertEqual(
+            TestMutableMapping({"10": 21, "30": 41, "a": 16, "b": 36, "11": 11}),
+            mapping,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
