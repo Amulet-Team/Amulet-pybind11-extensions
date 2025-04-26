64c7498c142aa18464865e1d498bf423588298e8
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-26 14:39:56 +0100

    Add sequence mixin functions

---
+++ tests/_test_sequence.py.cpp
@@ -0,0 +1,35 @@
+#include <pybind11/pybind11.h>
+
+#include <amulet/pybind11_extensions/sequence.hpp>
+
+namespace py = pybind11;
+namespace pyext = Amulet::pybind11_extensions;
+
+namespace detail {
+class TestSequence {
+public:
+    int get(int index)
+    {
+        return index;
+    }
+    size_t size()
+    {
+        return 5;
+    }
+};
+}
+
+PYBIND11_MODULE(_test_sequence, m)
+{
+    py::class_<detail::TestSequence> TestSequence(m, "TestSequence");
+    TestSequence.def(py::init());
+    TestSequence.def("__getitem__", &detail::TestSequence::get);
+    TestSequence.def("__len__", &detail::TestSequence::size);
+    pyext::collections::def_Sequence_getitem_slice(TestSequence);
+    pyext::collections::def_Sequence_contains(TestSequence);
+    pyext::collections::def_Sequence_iter(TestSequence);
+    pyext::collections::def_Sequence_reversed(TestSequence);
+    pyext::collections::def_Sequence_index(TestSequence);
+    pyext::collections::def_Sequence_count(TestSequence);
+    pyext::collections::register_Sequence(TestSequence);
+}
---
+++ tests/test_sequence.py
@@ -0,0 +1,60 @@
+import unittest
+from collections.abc import Sequence
+
+
+class SequenceTestCase(unittest.TestCase):
+    def test_sequence(self) -> None:
+        from _test_sequence import TestSequence
+
+        sequence = TestSequence()
+        self.assertEqual(5, len(sequence))
+
+        self.assertEqual(0, sequence[0])
+        self.assertEqual(1, sequence[1])
+        self.assertEqual(2, sequence[2])
+        self.assertEqual(3, sequence[3])
+        self.assertEqual(4, sequence[4])
+
+        self.assertEqual([2, 3], sequence[2:4])
+
+        self.assertNotIn(-1, sequence)
+        self.assertIn(0, sequence)
+        self.assertIn(1, sequence)
+        self.assertIn(2, sequence)
+        self.assertIn(3, sequence)
+        self.assertIn(4, sequence)
+        self.assertNotIn(5, sequence)
+
+        it = iter(sequence)
+        self.assertEqual(0, next(it))
+        self.assertEqual(1, next(it))
+        self.assertEqual(2, next(it))
+        self.assertEqual(3, next(it))
+        self.assertEqual(4, next(it))
+
+        it = reversed(sequence)
+        self.assertEqual(4, next(it))
+        self.assertEqual(3, next(it))
+        self.assertEqual(2, next(it))
+        self.assertEqual(1, next(it))
+        self.assertEqual(0, next(it))
+
+        self.assertEqual(0, sequence.index(0))
+        self.assertEqual(1, sequence.index(1))
+        self.assertEqual(2, sequence.index(2))
+        self.assertEqual(3, sequence.index(3))
+        self.assertEqual(4, sequence.index(4))
+
+        self.assertEqual(0, sequence.count(-1))
+        self.assertEqual(1, sequence.count(0))
+        self.assertEqual(1, sequence.count(1))
+        self.assertEqual(1, sequence.count(2))
+        self.assertEqual(1, sequence.count(3))
+        self.assertEqual(1, sequence.count(4))
+        self.assertEqual(0, sequence.count(5))
+
+        self.assertIsInstance(sequence, Sequence)
+
+
+if __name__ == "__main__":
+    unittest.main()
