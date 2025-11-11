07b96a27f6d44a3700ab004d8a079f950d8d208c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-11 15:26:30 +0000

    Add sequence binding function
    This makes it easy to return an ABC Sequence

--- src/amulet/pybind11_extensions/sequence.hpp
+++ src/amulet/pybind11_extensions/sequence.hpp
@@ -275,6 +275,71 @@ namespace pybind11_extensions {
             throw pybind11::index_error();
         }
     }
+
+    namespace detail {
+        template <typename SequenceT>
+        class SequenceWrapper {
+        public:
+            using SequenceType = SequenceT;
+
+            SequenceT& sequence;
+
+            SequenceWrapper(SequenceT& sequence)
+                : sequence(sequence)
+            {
+            }
+        };
+
+        template <typename SequenceWrapperT, typename ClsT>
+        void bind_sequence_to(ClsT& cls)
+        {
+            using T = typename SequenceWrapperT::SequenceType::value_type;
+            cls.def(
+                "__getitem__",
+                [](
+                    SequenceWrapperT& self,
+                    Py_ssize_t index) {
+                    bounds_check(self.sequence.size(), index);
+                    return self.sequence[index];
+                },
+                pybind11::arg("index"));
+            cls.def(
+                "__len__",
+                [](SequenceWrapperT& self) {
+                    return self.sequence.size();
+                });
+            
+            using Sequence = collections::Sequence<T>;
+            Sequence::def_repr(cls);
+            Sequence::def_getitem_slice(cls);
+            Sequence::def_contains(cls);
+            Sequence::def_iter(cls);
+            Sequence::def_reversed(cls);
+            Sequence::def_index(cls);
+            Sequence::def_count(cls);
+            Sequence::register_cls(cls);
+        }
+
+        template <typename SequenceWrapperT>
+        void bind_sequence()
+        {
+            pybind11::class_<SequenceWrapperT> Sequence(pybind11::handle(), "Sequence", pybind11::module_local());
+            bind_sequence_to<SequenceWrapperT>(Sequence);
+        }
+    }
+
+    // Make a python class that models collections.abc.Sequence around a C++ vector-like object.
+    // The caller must tie the lifespan of the sequence to the lifespan of the returned object.
+    template <typename SequenceT>
+    collections::Sequence<typename SequenceT::value_type> make_sequence(SequenceT& sequence)
+    {
+        using SequenceWrapperT = detail::SequenceWrapper<SequenceT>;
+        if (!is_class_bound<SequenceWrapperT>()) {
+            detail::bind_sequence<SequenceWrapperT>();
+        }
+        return pybind11::cast(SequenceWrapperT(sequence));
+    }
+
 } // namespace pybind11_extensions
 } // namespace Amulet
 
